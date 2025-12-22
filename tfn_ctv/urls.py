4eec79f9203a2064e1276332ef76ecbbbc72ee7c
AJ Javadi <amirjavadi25@gmail.com>
2025-12-22 10:47:43 -0600

    Update project settings and URL configuration for Church Financial ERP
    This commit modifies the `settings.py` and `urls.py` files to reflect the transition to the Church Financial ERP project. Key changes include updating project descriptions, integrating Wagtail CMS settings and applications, and restructuring URL patterns to accommodate the new admin and document routes. Unused Django CMS configurations have been commented out for clarity, and security settings have been refined for production readiness.

--- tfn_ctv/urls.py
+++ tfn_ctv/urls.py
@@ -1,38 +1,37 @@
 """
-URL configuration for tfn_ctv project.
+URL configuration for Church Financial ERP project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.contrib.auth.models import User
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import routers, serializers, viewsets
-from django.conf.urls.i18n import i18n_patterns
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('music_beta.urls')),  # Changed from 'music/' to root
+    # Django admin
+    path('django-admin/', admin.site.urls),
+    
+    # Wagtail admin
+    path('admin/', include(wagtailadmin_urls)),
+    
+    # Wagtail documents
+    path('documents/', include(wagtaildocs_urls)),
+    
+    # Church ERP URLs
+    path('', include('church_erp.urls')),
+    
+    # Wagtail pages (must be last)
+    path('', include(wagtail_urls)),
 ]
 
-# Add Django CMS URLs (commented out - install django-cms packages to enable)
-# urlpatterns += i18n_patterns(
-#     re_path(r'^', include('cms.urls')),
-# )
-
-# Add static and media URL mappings in development
+# Serve media files in development
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
